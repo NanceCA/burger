dce32faccdb873a9cda90e13a04b5fd6efd38432
nance <48306129+NanceCA@users.noreply.github.com>
2019-06-29 15:47:21 -0700

    adding additional variables

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,20 @@
 //creating the database connection
 var mysql = require("mysql");
-require('dotenv').config()
+var connection;
+require('dotenv').config();
 
-
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: process.env.PASSWORD,
-    database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: process.env.PASSWORD,
+        database: "burgers_db"
+    });
+}
 
 connection.connect(function (err) {
     if (err) {
