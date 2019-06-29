3aa74501f26f004b53d47211d40ba00ef693798a
nance <48306129+NanceCA@users.noreply.github.com>
2019-06-29 14:38:11 -0700

    adding connections

---
+++ config/connection.js
@@ -0,0 +1,23 @@
+//creating the database connection
+var mysql = require("mysql");
+require('dotenv').config()
+
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: process.env.PASSWORD,
+    database: "burgers_db"
+});
+
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connection: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId)
+});
+
+//exporting connection
+module.exports = connection;
